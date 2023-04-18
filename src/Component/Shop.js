6d18312b5bef86c25ec4aa360ad596f4c5f1455d
MrStark1234 <117528760+MrStark1234@users.noreply.github.com>
2023-04-18 14:17:52 +0530

    Initialize an app for implementing Redux

---
+++ src/Component/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Buy Reebok Shoes at 2500 Rs</h2>
+      <button className="btn btn-primary btn-sm mx-2">-</button>
+      Add to Cart
+      <button className="btn btn-primary btn-sm mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
