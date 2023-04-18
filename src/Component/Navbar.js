bcd04e99354748ba99c70c8f53f8a9c6ef119687
MrStark1234 <117528760+MrStark1234@users.noreply.github.com>
2023-04-18 15:48:11 +0530

    Redux Setup completed

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const amount = useSelector((state) => state.amount);
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
@@ -32,8 +34,8 @@ const Navbar = () => {
             </li>
           </ul>
           <div>
-            <button disabled="true" className="btn-primary">
-              Your Balance: 10000
+            <button disabled={true} className="btn-primary">
+              Your Balance: {amount}
             </button>
           </div>
         </div>
--- src/Component/Shop.js
+++ src/Component/Shop.js
@@ -1,12 +1,51 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators } from "../State/index";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+  const { withdrawMoney, depositeMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
   return (
     <div>
-      <h2>Buy Reebok Shoes at 2500 Rs</h2>
-      <button className="btn btn-primary btn-sm mx-2">-</button>
-      Add to Cart
-      <button className="btn btn-primary btn-sm mx-2">+</button>
+      <h2>Deposite/Withdraw Money</h2>
+      {/* <button
+        className="btn btn-primary btn-sm mx-2"
+        onClick={() => {
+          dispatch(actionCreators.withdrawMoney(100));
+        }}
+      >
+        -
+      </button>
+      Update Balance
+      <button
+        className="btn btn-primary btn-sm mx-2"
+        onClick={() => {
+          dispatch(actionCreators.depositeMoney(100));
+        }}
+      >
+        +
+      </button> */}
+      <button
+        className="btn btn-primary btn-sm mx-2"
+        onClick={() => {
+          withdrawMoney(100);
+        }}
+      >
+        -
+      </button>
+      Update Balance
+      <button
+        className="btn btn-primary btn-sm mx-2"
+        onClick={() => {
+          depositeMoney(100);
+        }}
+      >
+        +
+      </button>
     </div>
   );
 };
